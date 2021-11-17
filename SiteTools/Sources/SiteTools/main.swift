693476e3b1d2ef2a185676a2427a55dea22aa0ad
Valerii Bezuglyi <valnocorner@gmail.com>
2021-11-17 14:10:40 +0300

    read arg and create enumerator

--- SiteTools/Sources/SiteTools/main.swift
+++ SiteTools/Sources/SiteTools/main.swift
@@ -1,1 +1,15 @@
-print("Hello, world!")
+import Foundation
+
+// absoulute for projects/valnoc.github.io
+let repoLocalAbsolutePath = CommandLine.arguments[1]
+    .replacingOccurrences(of: "~", with: NSHomeDirectory())
+
+let fileManager = FileManager.default
+
+guard let resourcesURLsEnumerator = fileManager.enumerator(atPath: repoLocalAbsolutePath + "/resources") else {
+    print("failed to create enumerator for resources folder")
+    throw NSError()
+}
+
+var resourcesURLs = [URL]()
+print(resourcesURLs)
