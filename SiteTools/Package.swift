4cea06cbc30d7d21cbffeecb2218332a01b1e973
Valerii Bezuglyi <valnocorner@gmail.com>
2021-11-17 14:09:00 +0300

    platform

--- SiteTools/Package.swift
+++ SiteTools/Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SiteTools",
+    platforms: [
+        .macOS(.v12)
+    ],
 //    products: [
 //        // Products define the executables and libraries a package produces, and make them visible to other packages.
 //        .executable(
